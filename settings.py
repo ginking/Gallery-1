ae6dd86ba3d040135ca48410c4ddac59eb976ac6
Philippe Normand <phil@base-art.net>
2012-06-09 13:44:14 -0700

    settings update (media path and copyright)

--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 
 
 GALLERY_SETTINGS={'items_by_page': 20,
-                  'media_path': '/home/phil/shot',
+                  'media_path': '/home/phil/scaled',
                   'author': 'Philippe Normand',
-                  'copyright': 'Copyright &copy; 2006-2009',
+                  'copyright': 'Copyright &copy; 2006-2012',
                   'static_path': '/home/phil/devel/gallery/static',
                   'recent_photos_time': 60*60*24*14,
                   'rel_url': '/gallery',
